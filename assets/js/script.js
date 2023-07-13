e2302e5c0890ee4f51b42321cd655535261fb007
wyattdronen <137117182+wyattdronen@users.noreply.github.com>
2023-07-13 09:33:26 -0500

    html/variables/styling

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//current date
+var currentDay = $("#currentDay");
+currentDay.text (moment().format('dddd, MMM Do YYYY'));
+    
+//current hour
+var currentHour = moment().hour();
+
+//format text
