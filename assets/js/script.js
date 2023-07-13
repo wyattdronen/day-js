bfceb230d00dabb77e881fc9c768bff78a1d31b7
wyattdronen <137117182+wyattdronen@users.noreply.github.com>
2023-07-13 12:29:36 -0500

    add html links/js/styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,31 @@ currentDay.text (moment().format('dddd, MMM Do YYYY'));
 var currentHour = moment().hour();
 
 //format text
+$(".time-block").each(function(){
+    //hour value for time blocks
+ var blockHour = $(this).attr("id").split("-")[1];
+    //saved value from local storage
+var textEntry = localStorage.getItem(blockHour);
+var textArea = $(this).find(".description");
+    textArea.val (textEntry);
+    //hour compaired to hours and class to colors
+if (blockHour < currentHour){
+    $(this).find(".description").addClass("past");
+}else if(blockHour == currentHour){
+    $(this).find(".description").addClass("present");
+ }else{
+    $(this).find(".description").addClass("future");
+    }
+ });
+
+
+//save button, save to local storage
+$(".saveBtn").on("click", function(){
+//key
+var key = $(this).parent().attr("id").split("-")[1];
+//value
+var value = $(this).parent().find(".description").val();
+localStorage.setItem(key,value);
+});
+
+    
